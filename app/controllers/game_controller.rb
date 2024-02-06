37753c3d84965dff182d0988b4309a94404e0e9a
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-02-06 21:14:09 +0000

    Final Commit

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,24 @@
+class GameController < ApplicationController
+  def rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_rock"})
+  end 
+
+  def paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_paper"})
+  end 
+
+  def scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    render({ :template => "game_templates/play_scissors"})
+  end 
+
+  def rules
+    render({ :template => "game_templates/rules"})
+  end 
+
+end 
